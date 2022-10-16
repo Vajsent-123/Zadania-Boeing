abad7731d324d473f079fe1441eb9fd0bbb722c7
Damian Jankowski <56222920+Vajsent-123@users.noreply.github.com>
2022-10-16 14:36:13 +0200

    Update zadanie3.py
    Changing complexity

--- zadanie3/zadanie3.py
+++ zadanie3/zadanie3.py
@@ -1,10 +1,15 @@
-
 def b_in_a(list_a: list, list_b: list) -> list:
-    my_list = []
-    for m in list_a:
-        if m in list_b:
-            my_list.append(True)
+    element_count = {}
+    true_or_false_list = []
+    for element in list_a:
+        element_count[element] = 1
+    for element in list_b:
+        if element in element_count:
+            element_count[element] += 1
+    for key, value in element_count.items():
+        if value > 1:
+            true_or_false_list.append(True)
         else:
-            my_list.append(False)
-    return my_list
+            true_or_false_list.append(False)
 
+    return true_or_false_list
