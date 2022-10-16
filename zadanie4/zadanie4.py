53962758a473a7971605c38cb2a9832c46efb907
Damian Jankowski <56222920+Vajsent-123@users.noreply.github.com>
2022-10-16 14:36:57 +0200

    Update zadanie4.py
    cosmetic changes

--- zadanie4/zadanie4.py
+++ zadanie4/zadanie4.py
@@ -5,7 +5,7 @@
 def models_in_codes(models: list, codes: dict) -> list:
     my_list = []
     for i in models:
-        if i in codes.keys():
+        if i in codes:
             my_list.append(codes[i])
     return my_list
 
