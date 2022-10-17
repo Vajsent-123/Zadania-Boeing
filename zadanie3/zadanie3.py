f1f48f31d0c6d1af9169014d31ad8bdabe0c35db
Damian Jankowski <56222920+Vajsent-123@users.noreply.github.com>
2022-10-17 13:54:01 +0200

    Update zadanie3.py v2
    Other solution

--- zadanie3/zadanie3.py
+++ zadanie3/zadanie3.py
@@ -1,15 +1,11 @@
 def b_in_a(list_a: list, list_b: list) -> list:
     element_count = {}
     true_or_false_list = []
-    for element in list_a:
-        element_count[element] = 1
     for element in list_b:
+        element_count[element] = 1
+    for element in list_a:
         if element in element_count:
-            element_count[element] += 1
-    for key, value in element_count.items():
-        if value > 1:
             true_or_false_list.append(True)
         else:
             true_or_false_list.append(False)
-
     return true_or_false_list
